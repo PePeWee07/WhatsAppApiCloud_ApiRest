b2ba31a66eddaf09753348e701cb3aa0c0052873
Pepe <pepewee07@gmail.com>
2025-04-08 11:34:40 -0500

    configurar zona horaria a "America/Guayaquil" en la aplicación y ajustar el formato de la marca de tiempo en el endpoint de verificación de salud

--- src/main/java/com/BackEnd/WhatsappApiCloud/App.java
+++ src/main/java/com/BackEnd/WhatsappApiCloud/App.java
@@ -1,5 +1,7 @@
 package com.BackEnd.WhatsappApiCloud;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Guayaquil"));
 		SpringApplication.run(App.class, args);
 	}
 
--- src/main/java/com/BackEnd/WhatsappApiCloud/controller/HealthController.java
+++ src/main/java/com/BackEnd/WhatsappApiCloud/controller/HealthController.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TimeZone;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     // ======================================================
-    //   Health Check
+    // Health Check
     // ======================================================
     @CrossOrigin(origins = "https://ia-sp-backoffice.ucatolica.cue.ec")
     @GetMapping("/api/health")
@@ -22,8 +23,13 @@ public class HealthController {
         Map<String, Object> response = new HashMap<>();
         response.put("status", "ok");
         response.put("message", "API is running.");
-        response.put("timestamp", new SimpleDateFormat("M/d/yyyy, h:mm:ss a").format(new Date()));
+
+        SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy, h:mm:ss a");
+        sdf.setTimeZone(TimeZone.getTimeZone("America/Guayaquil"));
+        
+        response.put("timestamp", sdf.format(new Date()));
+
         return ResponseEntity.ok(response);
     }
-    
+
 }
