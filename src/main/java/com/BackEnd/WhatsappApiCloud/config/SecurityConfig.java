b549b5a8f4a79595d93254d5399acee9f7040142
Pepe <pepewee07@gmail.com>
2025-05-28 14:49:46 -0500

    Eliminar importación no utilizada en SecurityConfig

--- src/main/java/com/BackEnd/WhatsappApiCloud/config/SecurityConfig.java
+++ src/main/java/com/BackEnd/WhatsappApiCloud/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import java.util.*;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.method.P;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
